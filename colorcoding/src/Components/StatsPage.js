8d4a92bd3ac64d75f676124cd8820998d93c3daf
Tarique Smith <tpsmith1992@gmail.com>
2021-08-23 17:52:34 -0400

    Final push for day

--- colorcoding/src/Components/StatsPage.js
+++ colorcoding/src/Components/StatsPage.js
@@ -6,12 +6,19 @@ function StatsPage({ userList }) {
     (a, b) => b["winArray"].length - a["winArray"].length
   );
 
+  let colorBox1;
+  let colorBox2;
+  let colorBox3;
+
+
   return (
     <table>
       <tr>
         <th>Profile Picture</th>
         <th>Username</th>
-        <th>Last Win Combo</th>
+        <th>1st Win Color</th>
+        <th>2nd Win Color</th>
+        <th>3rd Win Color</th>
         <th>Last Win Time</th>
         <th>Last Comment</th>
         <th>Win Count</th>
@@ -27,7 +34,15 @@ function StatsPage({ userList }) {
           <td>
             <strong>{user.userName}</strong>
           </td>
-          <td>{user.winArray[0].WinCombo[0]} {user.winArray[0].WinCombo[1]} {user.winArray[0].WinCombo[0]}</td>
+          <td style={{background: `${user.winArray[0].WinCombo[0]}`}}>
+            {user.winArray[0].WinCombo[0]}
+          </td>
+          <td style={{background: `${user.winArray[0].WinCombo[1]}`}}>
+            {user.winArray[0].WinCombo[1]}
+          </td>
+          <td style={{background: `${user.winArray[0].WinCombo[2]}`}}>
+            {user.winArray[0].WinCombo[2]}
+          </td>
           <td>{user.winArray[0].winTime} seconds</td>
           <td>
             <em>{user.winArray[0].comment}</em>
