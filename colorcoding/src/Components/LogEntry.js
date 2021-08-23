2aa53cae6a5359135ff7ad4e48884d0a480f3ee2
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-08-23 17:52:03 -0400

    fixed key error on log

--- colorcoding/src/Components/LogEntry.js
+++ colorcoding/src/Components/LogEntry.js
@@ -1,8 +1,12 @@
 function LogEntry({entry}) {
+    let colorEntryKey = 0;
     return (
         <div className="log-entry">
             Entry {entry.id}:{" "}
-            {entry.combo.map((color,index)=><span key={color}>{color}{index===entry.combo.length ? "" : ", "}</span>)}
+            {entry.combo.map((color,index)=>{
+                colorEntryKey++;
+                return <span key={colorEntryKey}>{color}{index===entry.combo.length ? "" : ", "}</span>
+            })}
         </div>
     )
 }
