d6b3eb1a1eb3bdf6bf73be4e2b5594dcc39876cf
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-08-23 11:23:26 -0400

    game page and initial outline

--- colorcoding/src/App.js
+++ colorcoding/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router } from "react-router-dom"
 import React,{ useState,useEffect } from "react"
 import LoginPage from './Components/LoginPage';
+import GamePage from './Components/GamePage';
 
 //Imported hooks from react-router dom and react. Created console.log of Fetch API request to
 //possibly place in state variable. 
@@ -19,22 +20,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <LoginPage />
+      <GamePage />
     </div>
   );
 }
---
+++ colorcoding/src/Components/GamePage.js
@@ -0,0 +1,18 @@
+function GamePage() {
+    return (
+    <div>
+        <h1>Crack the Code!</h1>
+    </div>
+    )
+}
+
+export default GamePage;
+
+/*
+
+create a template for the page
+--create a window area for instructions and alerts
+--create an input area for selecting and inputting the color codes
+--create a log area for logging the inputs
+
+*/
