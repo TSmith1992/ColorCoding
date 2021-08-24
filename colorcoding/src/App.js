1e60fb12c20b387a4d6a44c2df0d392809cbb328
Tarique Smith <tpsmith1992@gmail.com>
2021-08-24 10:35:25 -0400

    Added checks to ensure user completes all fields before playing

--- colorcoding/src/App.js
+++ colorcoding/src/App.js
@@ -1,46 +1,65 @@
-import './App.css';
-import React,{ useState,useEffect } from "react"
-import LoginPage from './Components/LoginPage';
-import StatsPage from './Components/StatsPage';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import LoginPage from "./Components/LoginPage";
+import StatsPage from "./Components/StatsPage";
 
 function App() {
-  const fetchAPI="http://localhost:3000/userList"
-  const [userList, setUserList] =useState([])
-  const [defaultLogin, setDefaultLogin] = useState({userName:"", profilePic:"", winArray:[] })
+  const fetchAPI = "http://localhost:3000/userList";
+  const [userList, setUserList] = useState([]);
+  const [defaultLogin, setDefaultLogin] = useState({
+    userName: "",
+    profilePic: "",
+    winArray: [],
+  });
 
   useEffect(() => {
     fetch(fetchAPI)
-    .then(r => r.json())
-    .then(uList => setUserList(uList))
-  }, [])
+      .then((r) => r.json())
+      .then((uList) => setUserList(uList));
+  }, []);
 
   //function to handle submission of new profile
-  function handleSubmit(e){
+  function handleSubmit(e) {
     //upon submission, user can be redirected to game page
     //did not keep e.prevenDefault() because page would be redirected to gaming page anyway
-    //e.preventDefault()
-    fetch(fetchAPI, {
-      method:"POST",
-      headers:{
-        "Content-Type": "application/json"
-      },
-      body : JSON.stringify(defaultLogin)
-    })
-    .then(r => r.json())
-    .then(data => console.log(data))
+    if (!defaultLogin.userName || !defaultLogin.profilePic) {
+      alert("Please complete all fields to play");
+    } else if (
+      userList.filter((player) => player.userName === defaultLogin.userName)
+    ) {
+      alert("This name has already been taken. Please choose another");
+    } else if (
+      userList.filter((player) => player.profilePic === defaultLogin.profilePic)
+    ) {
+      alert("This picture is already in use. Please choose another");
+    } else {
+      fetch(fetchAPI, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(defaultLogin),
+      })
+        .then((r) => r.json())
+        .then((data) => console.log("POSTed"));
+    }
   }
-  
+
   //function to create new profile
-  function onAddNewUser(e){
-    const name=e.target.name;
-    let value=e.target.value;
-    setDefaultLogin({...defaultLogin, [name]:value})
+  function onAddNewUser(e) {
+    const name = e.target.name;
+    let value = e.target.value;
+    setDefaultLogin({ ...defaultLogin, [name]: value });
   }
 
   return (
     <div className="App">
-      <LoginPage onAddNewUser={onAddNewUser} defaultLogin={defaultLogin} handleSubmit={handleSubmit}/>
-      <StatsPage userList={userList}/>
+      <LoginPage
+        onAddNewUser={onAddNewUser}
+        defaultLogin={defaultLogin}
+        handleSubmit={handleSubmit}
+      />
+      <StatsPage userList={userList} />
     </div>
   );
 }
--- colorcoding/src/Components/StatsPage.js
+++ colorcoding/src/Components/StatsPage.js
@@ -1,53 +1,58 @@
 import React from "react";
 
-//<>{leaderBoard.map(user => < div key={user.id}>{user["winArray"].length}</div>)}</>;
 function StatsPage({ userList }) {
   const leaderBoard = userList.sort(
     (a, b) => b["winArray"].length - a["winArray"].length
   );
 
-  const leaderBoardWin = leaderBoard.filter(player =>player.winArray.length>0)
+  const leaderBoardWin = leaderBoard.filter(
+    (player) => player.winArray.length > 0
+  );
 
   return (
     <table>
-      <tr>
-        <th>Profile Picture</th>
-        <th>Username</th>
-        <th>1st Win Color</th>
-        <th>2nd Win Color</th>
-        <th>3rd Win Color</th>
-        <th>Last Win Time</th>
-        <th>Last Comment</th>
-        <th>Win Count</th>
-      </tr>
-      {leaderBoardWin.map((user) => (
-        <tr key={user.id}>
-          <td>
-            <img
-              src={user.profilePic}
-              style={{ height: "300px", width: "300px" }}
-              alt="Profile Pic"
-            />
-          </td>
-          <td>
-            <strong>{user.userName}</strong>
-          </td>
-          <td style={{ background: `${user.winArray[0].WinCombo[0]}` }}>
-            {user.winArray[0].WinCombo[0]}
-          </td>
-          <td style={{ background: `${user.winArray[0].WinCombo[1]}` }}>
-            {user.winArray[0].WinCombo[1]}
-          </td>
-          <td style={{ background: `${user.winArray[0].WinCombo[2]}` }}>
-            {user.winArray[0].WinCombo[2]}
-          </td>
-          <td>{user.winArray[0].winTime} seconds</td>
-          <td>
-            <em>{user.winArray[0].comment}</em>
-          </td>
-          <td>{user.winArray.length}</td>
+      <thead>
+        <tr>
+          <th>Profile Picture</th>
+          <th>Username</th>
+          <th>1st Win Color</th>
+          <th>2nd Win Color</th>
+          <th>3rd Win Color</th>
+          <th>Last Win Time</th>
+          <th>Last Comment</th>
+          <th>Win Count</th>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {leaderBoardWin.map((user) => (
+          <tr key={user.id}>
+            <td>
+              <img
+                src={user.profilePic}
+                style={{ height: "300px", width: "300px" }}
+                alt="Profile Pic"
+              />
+            </td>
+            <td>
+              <strong>{user.userName}</strong>
+            </td>
+            <td style={{ background: `${user.winArray[0].WinCombo[0]}` }}>
+              {user.winArray[0].WinCombo[0]}
+            </td>
+            <td style={{ background: `${user.winArray[0].WinCombo[1]}` }}>
+              {user.winArray[0].WinCombo[1]}
+            </td>
+            <td style={{ background: `${user.winArray[0].WinCombo[2]}` }}>
+              {user.winArray[0].WinCombo[2]}
+            </td>
+            <td>{user.winArray[0].winTime} seconds</td>
+            <td>
+              <em>{user.winArray[0].comment}</em>
+            </td>
+            <td>{user.winArray.length}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
